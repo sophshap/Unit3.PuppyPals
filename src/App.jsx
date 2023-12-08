ab7bb7f3a23cdbd81050292e08e433c07c8d2695
sophshap <sophieashapiro@gmail.com>
2023-12-08 15:14:54 -0500

    puppy names rendered to screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import { useState } from 'react'
-
+import { puppyList } from './data.js'
 import './App.css'
 
 function App() {
- 
+  const [puppies, setPuppies] = useState(puppyList)
 
   return (
     <>
       <div>
-       
+        {
+          puppies.map((puppy) => {
+            return <p key={puppy.id}>{puppy.name}</p>;
+           })
+        }
+
+
       </div>
-      
+
     </>
   )
 }
