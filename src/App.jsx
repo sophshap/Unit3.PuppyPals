0a9dfd0b634331c2b1f743a7e4a44ea22fabe2b1
sophshap <sophieashapiro@gmail.com>
2023-12-08 16:12:06 -0500

    finished workshop, working code

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,32 @@ import './App.css'
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList)
+  const [featPupId, setFeatPupId] = useState(null)
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId)
+  console.log(featuredPup)
 
   return (
+
     <>
       <div>
         {
           puppies.map((puppy) => {
-            return <p key={puppy.id}>{puppy.name}</p>;
-           })
+
+            return <p onClick={() => { setFeatPupId(puppy.id) }} key={puppy.id}>{puppy.name}</p>
+          })
+
         }
 
+        {featPupId && (
+          <div>
+            <h2>{featuredPup.name}</h2>
+            <ul>
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+          </div>
+        )}
 
       </div>
 
